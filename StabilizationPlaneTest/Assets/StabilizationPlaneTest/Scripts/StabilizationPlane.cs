8c6e5827094f73abbe63383234204fe6d2355f0b
shinya <shinya@hololab.co.jp>
2018-03-04 02:41:42 +0900

    1st Commit

---
+++ StabilizationPlaneTest/Assets/StabilizationPlaneTest/Scripts/StabilizationPlane.cs
@@ -0,0 +1,55 @@
+﻿using UnityEngine;
+
+namespace HoloLens.StabilizationTest {
+    public class StabilizationPlane : MonoBehaviour {
+
+        /// <summary>
+        /// The object you would like to be stable
+        /// </summary>
+        public GameObject focusedObject;
+        /// <summary>
+        /// The flag to show dammy stabilization plane.
+        /// </summary>
+        public bool IsVisible;
+
+        private Renderer planeRenderer;
+
+        private Renderer PlaneRenderer {
+            get {
+                if (planeRenderer == null) {
+                    planeRenderer = GetComponent<Renderer>();
+                }
+                return planeRenderer;
+            }
+        }
+
+        void Update() {
+            UpdateStabilizationPlane();
+        }
+
+        /// <summary>
+        /// Update Stabilization Plane position and normal
+        /// </summary>
+        private void UpdateStabilizationPlane() {
+
+            if (focusedObject == null) {
+                if (PlaneRenderer.enabled == true) { PlaneRenderer.enabled = false; }
+                return;
+            }
+
+            var normal = -Camera.main.transform.forward;
+            var position = focusedObject.transform.position;
+            UnityEngine.XR.WSA.HolographicSettings.SetFocusPointForFrame(position, normal);
+
+            if (IsVisible) {
+                if (PlaneRenderer.enabled == false) { PlaneRenderer.enabled = true; return; }
+                transform.forward  = normal;
+                transform.position = position;
+            }
+            else {
+                if (PlaneRenderer.enabled == true) { PlaneRenderer.enabled = false; }
+            }
+        }
+    }
+
+}
---
+++ StabilizationPlaneTest/Assets/StabilizationPlaneTest/Scripts/StabilizeTargetSetter.cs
@@ -0,0 +1,52 @@
+﻿using UnityEngine;
+using UnityEngine.XR.WSA.Input;
+
+namespace HoloLens.StabilizationTest {
+    [RequireComponent(typeof(StabilizationPlane))]
+    public class StabilizeTargetSetter : MonoBehaviour {
+
+        StabilizationPlane stabilizationPlane;
+        StabilizationPlane StabilizationPlane {
+            get {
+                if (stabilizationPlane == null) {
+                    stabilizationPlane = GetComponent<StabilizationPlane>();
+                }
+
+                return stabilizationPlane;
+            }
+
+            set { stabilizationPlane = value; }
+        }
+
+        // Use this for initialization
+        void Start() {
+            InteractionManager.InteractionSourcePressed += OnTapped;
+        }
+
+        private void Update() {
+            if (Input.GetKeyDown(KeyCode.Mouse0)) {
+                var obj = GetTappedObject();
+                if (obj != null) { StabilizationPlane.focusedObject = obj; }
+            }
+        }
+
+        private void OnTapped(InteractionSourcePressedEventArgs e) {
+            var obj = GetTappedObject();
+            if(obj != null) { StabilizationPlane.focusedObject = obj; }
+        }
+
+        private GameObject GetTappedObject() {
+            Ray ray = new Ray(Camera.main.transform.position, Camera.main.transform.forward);
+            RaycastHit hit;
+            if (Physics.Raycast(ray, out hit, Mathf.Infinity)) {
+                Debug.LogFormat("Hit:{0}",hit.transform.name);
+                return hit.transform.gameObject;
+            }
+            else {
+                Debug.Log("No Hit");
+                return null;
+            }
+        }
+    }
+
+}
